9162e91ec84ebec3cd93cb2621a9d113b5eafef9
ryu <roksellid01@gmail.com>
2021-10-25 21:22:39 +0900

    ㅡ

--- src/semiProject/Test.java
+++ src/semiProject/Test.java
@@ -3,40 +3,6 @@ package semiProject;
 public class Test {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("hi 희"); 
-=======
-
-		System.out.println("hi 가희"); 
-		System.out.println("hi 가희"); 
-		System.out.println("hi 유가"); 
-
-		System.out.println("hi 온화");
-		System.out.println("hi 온화");
-		System.out.println("hi 가희"); 
-		System.out.println("hi 가희"); 
-		System.out.println("hi 가희");
-		System.out.println("어려워요_나현");
-		System.out.println("git.._나현");
-		System.out.println("hi 가희"); 
-		System.out.println("hi 가희");
-		System.out.println("hi 가희"); 
-		System.out.println("hi 가희");
-		System.out.println("hi 가희"); 
-		System.out.println("hi 가희");
-		System.out.println("hi 가희"); 
-		System.out.println("hi 가희");
-		System.out.println("이지희");
-		System.out.println("왜 됐다가 안 됐다가 해ㅠㅠㅠㅠㅠㅠㅠ 짜증");
-
-		System.out.println("hi 유가2222"); 
-
-		System.out.println("hi 가2희"); 
-		System.out.println("hi 가2희2"); 
-		System.out.println("hi 유2가2"); 
-		System.out.println("현");
-		
->>>>>>> branch 'master' of https://github.com/chihee721/SemiProject
-
 	}
 }
