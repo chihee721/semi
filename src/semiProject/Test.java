ae541710476ae87131dd7ef3bfc66d15b714097b
Jeongonhwa <onhwav4432@gmail.com>
2021-10-26 02:45:17 +0900

    테스트

--- src/semiProject/Test.java
+++ src/semiProject/Test.java
@@ -4,13 +4,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 
-		System.out.println("hi 가희"); 
-
-
-		System.out.println("dfasdf");
-		
-		System.out.println("ddddddddddddfasdfszfddddddddddddddd");
-
+		System.out.println("테스트 합니다!"); 
 		
 	}
 
