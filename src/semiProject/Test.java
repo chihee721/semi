e4a17ee9c1009d255bc98295933538c742a0094b
유가희 <ygh9040@naver.com>
2021-10-25 21:21:50 +0900

    ㅋㅋ

--- src/semiProject/Test.java
+++ src/semiProject/Test.java
@@ -3,15 +3,9 @@ package semiProject;
 public class Test {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("hi 가희"); 
-		System.out.println("hi 가희"); 
-		System.out.println("hi 유가"); 
-		System.out.println("hi 유가2222"); 
-=======
 		System.out.println("hi 가2희"); 
 		System.out.println("hi 가2희2"); 
 		System.out.println("hi 유2가2"); 
->>>>>>> branch 'master' of https://github.com/chihee721/SemiProject.git
+
 	}
 }
