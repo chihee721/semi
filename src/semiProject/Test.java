22ac8242acc4d5397b3a6d1fe318796f2be3488a
ryu <roksellid01@gmail.com>
2021-10-25 20:57:36 +0900

    마스터_충돌

--- src/semiProject/Test.java
+++ src/semiProject/Test.java
@@ -3,8 +3,8 @@ package semiProject;
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("hi 가희"); 
-		System.out.println("hi 가희"); 
-		System.out.println("hi 유가"); 
+		System.out.println("hi 가2희"); 
+		System.out.println("hi 가2희2"); 
+		System.out.println("hi 유2가2"); 
 	}
 }
