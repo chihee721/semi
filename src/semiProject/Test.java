e9eefb9cc9d0cabde23f332ac88841f34d1b20e8
Jeongonhwa <onhwav4432@gmail.com>
2021-10-25 20:25:52 +0900

    테스트

--- src/semiProject/Test.java
+++ src/semiProject/Test.java
@@ -6,6 +6,9 @@ public class Test {
 		System.out.println("hi 가희"); 
 		System.out.println("hi 유가"); 
 		System.out.println("hi 온화");
+		System.out.println("hi 유가"); 
+		System.out.println("hi 온화");
+		System.out.println("hi 온화");
 	}
 
 }
